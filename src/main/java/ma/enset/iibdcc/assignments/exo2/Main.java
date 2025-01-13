bcd2b936717267ea1cf472ecd8caa81797e99966
Tidiane DIALLO <tidiane.diallo@teliman.ml>
2025-01-13 10:31:18 +0100

    Adding waiting until threads finish before calculating the sum

--- src/main/java/ma/enset/iibdcc/assignments/exo2/Main.java
+++ src/main/java/ma/enset/iibdcc/assignments/exo2/Main.java
@@ -2,17 +2,18 @@ package ma.enset.iibdcc.assignments.exo2;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         int somme = 0;
         int length = 100_000;
-        int tableSplit = 5;
+        int tableSplit = 20;
         int[] array = new int[length];
         Sommeur[] sommeurs = new Sommeur[tableSplit];
         ExecutorService executorService = Executors.newFixedThreadPool(tableSplit);
         for (int i = 0; i < length; i++) {
-            array[i] = i*2;
+            array[i] = i;
         }
         for (int i = 0; i < tableSplit; i++) {
             int startIndex = i * (length / tableSplit);
@@ -22,11 +23,21 @@ public class Main {
             executorService.execute(sommeur);
         }
         executorService.shutdown();
-        for (int i = 0; i < tableSplit; i++) {
-            somme += sommeurs[i].getSomme();
-//            System.out.println(sommeurs[i].toString());
+
+        try {
+            boolean finished = executorService.awaitTermination(1, TimeUnit.MINUTES);
+            if (finished) {
+                for (int i = 0; i < tableSplit; i++) {
+                    somme += sommeurs[i].getSomme();
+                }
+                int rightSum = (length * (length - 1)) / 2;
+                System.out.println("Somme finale : " + somme + " Right sum : " + rightSum + " diff : " + (rightSum - somme));
+            }else {
+                System.out.println("Not all threads finished in time");
+            }
+        } catch (InterruptedException e) {
+            e.getStackTrace();
         }
-        System.out.println("Somme finale : " + somme);
 
     }
 }
